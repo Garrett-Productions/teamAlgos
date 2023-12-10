b247c8524b8d38a96ccc9e9ed951a4f29c47d821
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-12-10 11:27:24 -0800

    first commit

--- week1/plusOne.js
+++ week1/plusOne.js
@@ -25,4 +25,5 @@ function plusOne(digits){
             // else first iteration is 9, so we set it equal to 0, then on our 2nd iteration we increase the second number and return it
         }
     }
+    //one more edge case to handle, if our input array is [9,9] then we need to return [1,0,0] and not [0,0]
 };
