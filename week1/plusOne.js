3d1931d1a4cd8e8011834c7430c3ad1ed3871f5f
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-12-10 11:26:03 -0800

    first commit

--- week1/plusOne.js
+++ week1/plusOne.js
@@ -13,3 +13,16 @@
 
 //Solving:
 //working through the array backwards, if the array is [1,2,9] we need to return [1,3,0], so if there is a 9 on the far left we need to go to the left
+
+function plusOne(digits){
+    for(let i = digits.length - 1; i >= 0; i--){
+        //if digits is less than 9, at this first iteration, increase the num, and return it
+        if(digits[i]< 9){
+            digits[i]= digits[i]+1;
+            return digits;
+        } else {
+            digits[i] = 0;
+            // else first iteration is 9, so we set it equal to 0, then on our 2nd iteration we increase the second number and return it
+        }
+    }
+};
